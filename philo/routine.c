332aa75927374f979f7fbcb8d341757073e4e6ba
Leonardo Maes <lmaes@c2r1s10.42porto.com>
2024-10-24 14:11:10 +0100

    Philosophers

--- philo/routine.c
+++ philo/routine.c
@@ -12,6 +12,20 @@
 
 #include "philosophers.h"
 
+
+
+int		phiosopher_dead(t_philo *philo, size_t	time_to_die)
+{
+	pthread_mutex_lock(philo->meal_lock);
+	if (get_current_time() - philo->last_eat > time_to_die /*&& philo->eating == 0*/)
+	{
+		pthread_mutex_unlock(philo->meal_lock);
+		return (1);
+	}
+	pthread_mutex_unlock(philo->meal_lock);
+	return (0);
+}
+
 void	*is_dead(void *void_program)
 {
 	t_program *program;
@@ -20,20 +34,19 @@ void	*is_dead(void *void_program)
 	program = (t_program *)void_program;
 	while (1)
 	{
-		ft_usleep(1);
+		//ft_usleep(1);
 		i = 0;
 		while (i < program->philo[0].n_philos)
-		{
-			pthread_mutex_lock(program->philo[0].dead_lock);
-			if (get_current_time() - program->philo[i].last_eat >= program->philo[i].time_to_die && program->philo[i].eating == 0)
+		{		// Problema na condicao quando o eating estiver 1 ou 0
+			if (phiosopher_dead(&program->philo[i], program->philo[i].time_to_die))		// && program->philo[i].eating == 0
 			{
+				pthread_mutex_lock(program->philo[i].dead_lock);
 				printf("%lums %i died\n", get_current_time() - program->philo[i].start_time, program->philo[i].id);
 				program->philo[i].dead = 1;
 				program->dead = 1;
-				pthread_mutex_unlock(program->philo[0].dead_lock);
+				pthread_mutex_unlock(program->philo[i].dead_lock);
 				return (NULL);
 			}
-			pthread_mutex_unlock(program->philo[0].dead_lock);
 			i++;
 		}
 	}
@@ -54,13 +67,15 @@ int	check_dead(t_philo *philo)
 
 void	eat(t_philo *philo)
 {
-	//pthread_mutex_lock(philo->meal_lock);
+	pthread_mutex_lock(philo->meal_lock);
 	philo->eating = 1;
-	printf("%lums %i is eating\n", get_current_time() - philo->start_time, philo->id);
+	printf("%lums %i is eating\n", get_current_time() - philo->start_time, philo->id);		//possivel erro na mutex aqui
 	philo->last_eat = get_current_time();
+	pthread_mutex_unlock(philo->meal_lock);
 	ft_usleep(philo->time_to_eat);
+	pthread_mutex_lock(philo->meal_lock);
 	philo->eating = 0;
-	//pthread_mutex_unlock(philo->meal_lock);
+	pthread_mutex_unlock(philo->meal_lock);
 }
 
 void	*routine(void	*philos)
@@ -71,11 +86,11 @@ void	*routine(void	*philos)
 
 	while (check_dead(philo) == 0)
 	{
+		//ft_usleep(1);
 		eat(philo);
 	}
 	return (NULL);
 }
-// Alerar toda a struct philo para dentro do overall, desta forma quando um filo morrer, altera a variavel do overall para parar todos os philos na funcao check_dead na routine
 
 int	start_routine(t_program *program)
 {
@@ -88,16 +103,17 @@ int	start_routine(t_program *program)
 	while (i < program->philo[0].n_philos)
 	{
 		if (pthread_create(&program->philo[i].philo, NULL, &routine, &program->philo[i]) != 0)
-			return 1;
+			return (1);
 		i++;
 	}
 	pthread_join(monitor, NULL);
 	i = 0;
 	while (i < program->philo[0].n_philos)
 	{
-		pthread_join(program->philo[i].philo, NULL);
+		if (pthread_join(program->philo[i].philo, NULL) != 0)
+			return (1);
 		i++;
 	}
-	program->dead = 2;
+	//program->dead = 2;
 	return (0);
 }
